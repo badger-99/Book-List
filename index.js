8294ddfe825da804b9bc48d3fda6153a9f30ff8c
Alfred Peter <alpeter91@gmail.com>
2023-06-07 01:36:54 +0300

    Add localStorage functionlity

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ let bookArray = [];
 
 
 
+
 const bookTamplate = (books, index) => `
 <div class="book">
 <p>${books.title}</p>
@@ -15,6 +16,14 @@ const bookTamplate = (books, index) => `
 <hr>
 </div>
 `;
+
+// Rendering the books
+let bookDataString = localStorage.getItem('bookData')
+let retrievedBooks = JSON.parse(bookDataString)
+let bookList = retrievedBooks
+  .map((book, index) => bookTamplate(book, index))
+  .join('');
+container.innerHTML = bookList;
 
 // ADD button event listener
 bookEntry
@@ -29,22 +38,46 @@ bookEntry
     book.title = title.value;
     book.author = author.value;
     bookArray.push(book);
+    title.value = '';
+    author.value = '';
     console.log(bookArray);
 
-    // Rendering the book
-    const bookList = bookArray
+    
+    //  updating localStorage
+    let bookData = JSON.stringify(bookArray);
+    localStorage.setItem('bookData', bookData);
+
+    //  retrieving from localStorage
+    bookDataString = localStorage.getItem('bookData');
+    retrievedBooks = JSON.parse(bookDataString);
+
+    console.log(retrievedBooks)
+
+    // Rendering the books
+    bookList = retrievedBooks
       .map((book, index) => bookTamplate(book, index))
       .join('');
     container.innerHTML = bookList;
-
   })
   
   // Remove button event listener
-container.addEventListener('click', (event) => {
-  if (event.target.classList.contains('remove')) {
-    let bookNum = event.target.dataset.index;
-    bookArray.splice(parseInt(bookNum, 10), 1);
-    let newBookArray = bookArray.map((book, index) => bookTamplate(book, index)).join('');
-    container.innerHTML = newBookArray;      
+  container.addEventListener('click', (event) => {
+    if (event.target.classList.contains('remove')) {
+      let bookNum = event.target.dataset.index;
+      bookArray.splice(parseInt(bookNum, 10), 1);
+
+      //  updating localStorage
+      let bookData = JSON.stringify(bookArray);
+      localStorage.setItem('bookData', bookData);
+
+      // retirieving from localStorage
+      bookDataString = localStorage.getItem('bookData');
+      retrievedBooks = JSON.parse(bookDataString);
+
+      bookList = retrievedBooks
+        .map((book, index) => bookTamplate(book, index))
+        .join('');
+      container.innerHTML = bookList;      
     }
   })
+  
