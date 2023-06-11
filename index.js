0105fa60faa740f8c0e70612db6508f9dc41de5c
Alfred Peter <alpeter91@gmail.com>
2023-06-11 17:09:15 +0300

    Reduce classes from 3 to 1 to fix eslint error

--- index.js
+++ index.js
@@ -2,20 +2,19 @@ const container = document.querySelector('#book-list');
 const bookEntry = document.querySelector('#book-input');
 const title = document.querySelector('#title');
 const author = document.querySelector('#author');
+const bookArray = [];
 
-// Book template
-const bookTemplate = (books, index) => `
+const bookTamplate = (books, index) => `
 <div class="book">
 <p>"${books.title}" by ${books.author}</p>
 <button type="button" class="remove" data-index="${index}">Remove</button>
 </div>
 `;
 
-// Helper functions
 class UtilityFunctions {
-  // Saving to Local Storage -> uses bookArray
-  static setStorage = (array) => {
-    localStorage.setItem('bookData', JSON.stringify(array));
+  // Saving to Local Storage
+  static setStorage = () => {
+    localStorage.setItem('bookData', JSON.stringify(bookArray));
   };
 
   // Getting from Local Storage
@@ -24,71 +23,48 @@ class UtilityFunctions {
     return retrievedBooks;
   };
 
-  // Showing books on browser  -> uses container
-  static showBooks = (element, template) => {
+  // Showing books on browser
+  static showBooks = () => {
     const retrievedBooks = this.getStorage();
-    element.innerHTML = retrievedBooks
-      .map((book, index) => template(book, index))
+    container.innerHTML = retrievedBooks
+      .map((book, index) => bookTamplate(book, index))
       .join('');
   };
 
-  static addBook(array, book) {
-    array.push(book);
-    this.setStorage(array)
+  // Add book method
+  static addBook() {
+    const book = {
+      title: title.value,
+      author: author.value,
+    };
+    bookArray.push(book);
+    this.setStorage();
   }
 
-  static removeBook(array, idx) {
-    array.splice(idx, 1);
-    this.setStorage(array);
-  }
-}
-
-// Book collection class
-class BookCollection {
-  constructor() {
-    this.array = [];
-  }
-
-  getArray() {
-    return this.array;
+  // Remove book method
+  static removeBook(idx) {
+    bookArray.splice(idx, 1);
+    this.setStorage();
   }
 }
 
-// Book class
-class Book{
-  constructor(title, author) {
-      this.title= title,
-      this.author= author
-  }
-
-  getBook() {
-    return { title: this.title, author: this.author };
-  }
-}
-
-// initializing new array, populating it with localStorage, and rendering it
-const bookList = new BookCollection()
-let bookArray = bookList.getArray()
-bookArray = [...UtilityFunctions.getStorage()]
-UtilityFunctions.showBooks(container, bookTemplate)
+bookArray.push(...UtilityFunctions.getStorage());
+UtilityFunctions.showBooks();
 
 // ADD button event listener
-bookEntry
-  .addEventListener('submit', (e) => {
-    e.preventDefault();
-    const book = new Book(title.value, author.value);
-    const bookObj = book.getBook();
-    bookEntry.reset();
-    UtilityFunctions.addBook(bookArray, bookObj);
-    UtilityFunctions.showBooks(container, bookTemplate);
-  });
-  
+bookEntry.addEventListener('submit', (e) => {
+  e.preventDefault();
+  UtilityFunctions.addBook();
+  bookEntry.reset();
+  UtilityFunctions.showBooks();
+});
+
 // Remove button event listener
 container.addEventListener('click', (event) => {
   if (event.target.classList.contains('remove')) {
     const bookNum = event.target.dataset.index;
-    idx = parseInt(bookNum, 10);
-    UtilityFunctions.removeBook(bookArray, idx);
-    UtilityFunctions.showBooks(container, bookTemplate);
+    const idx = parseInt(bookNum, 10);
+    UtilityFunctions.removeBook(idx);
+    UtilityFunctions.showBooks();
   }
 });
