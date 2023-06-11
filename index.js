bd46fada3e8a9ee2084cefb372d4fc8c7249e663
Alfred Peter <alpeter91@gmail.com>
2023-06-11 21:05:07 +0300

    Add live clock

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const list = document.getElementById('list');
 const addNew = document.getElementById('add-new');
 const contact = document.getElementById('contact');
 const heading = document.getElementById('header-name');
+const dateDiv = document.getElementById('date')
 const title = document.querySelector('#title');
 const author = document.querySelector('#author');
 const bookArray = [];
@@ -98,3 +99,21 @@ contact.addEventListener('click', (e) => {
   contactDetails.classList.remove('hidden');
   heading.innerHTML = 'Contact information';
 });
+
+// Live clock
+const weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
+const leadingZero = (number) => {
+  if (number < 10) {
+    return `0${number}`
+  }
+  return number
+}
+const liveClock = () => {
+  const date = new Date();
+  const time = `${weekdays[date.getDay()]} ${months[date.getMonth()]} ${leadingZero(date.getDate())} ${date.getFullYear()} ${leadingZero(date.getHours())}:${leadingZero(date.getMinutes())}:${leadingZero(date.getSeconds())}`;
+  dateDiv.innerHTML = time;  
+};
+
+setInterval(liveClock, 1000)
